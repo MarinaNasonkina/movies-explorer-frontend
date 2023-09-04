38a869904968e7424d2a62065538d7511ec8a085
MarinaNasonkina <marina.nasonkina96@yandex.ru>
2023-09-04 18:32:43 +0300

    feat: страница 404

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function PageNotFound() {
   return (
-    <></>
+    <main className='page-not-found'>
+      <h1 className='page-not-found__title'>404</h1>
+      <p className='page-not-found__subtitle'>Страница не найдена</p>
+      <Link to='/' className='link page-not-found__link'>Назад</Link>
+    </main>
   );
 }
