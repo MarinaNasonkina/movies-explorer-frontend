1f2bb7a6e8d681ed479d3cc996b61bd37234f268
MarinaNasonkina <marina.nasonkina96@yandex.ru>
2023-09-08 18:27:53 +0300

    feat: переход назад в PageNotFound

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 import './PageNotFound.css';
 
 export default function PageNotFound() {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
   return (
     <main className='page-not-found'>
       <h1 className='page-not-found__title'>404</h1>
       <p className='page-not-found__subtitle'>Страница не найдена</p>
-      <Link to='/' className='page-not-found__link'>Назад</Link>
+      <Link onClick={goBack} to='#' className='page-not-found__link'>Назад</Link>
     </main>
   );
 }
